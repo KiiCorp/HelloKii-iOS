df21859cd2d19a700e9c95f4909bf6f4d0aa0d1b
rizakii <riza.alaudin@kii.com>
2016-10-31 11:43:34 +0900

    xcode 8 conversion

--- HelloKii-iOS-Swift/HelloKii-iOS-Swift/MainViewController.swift
+++ HelloKii-iOS-Swift/HelloKii-iOS-Swift/MainViewController.swift
@@ -24,14 +24,14 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
 
     // define the loaded KiiObject
-    private var objectList: [KiiObject] = []
+    fileprivate var objectList: [KiiObject] = []
 
     // define the object count
     // used to easily see object names incrementing
-    private var objectCount = 0
+    fileprivate var objectCount = 0
 
-    private let bucketName = "myBucket"
-    private let objectKey = "myObjectValue"
+    fileprivate let bucketName = "myBucket"
+    fileprivate let objectKey = "myObjectValue"
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -45,14 +45,14 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLayoutSubviews()
 
         // add "+" button to the navigation bar
-        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "addItem")
+        let addButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(MainViewController.addItem))
         navigationItem.rightBarButtonItem = addButton
 
         // initialize the activity indicator to display on the top of the screen
         self.activityIndicator.layer.zPosition = 1
     }
 
-    override func viewDidAppear(animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         // show the activity indicator
         activityIndicator.startAnimating()
 
@@ -61,62 +61,62 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
 
         // create an empty KiiQuery (will retrieve all results, sorted by creation date)
         let allQuery = KiiQuery(clause: nil)
-        allQuery.sortByDesc("_created")
+        allQuery.sort(byDesc: "_created")
 
         // define the bucket to query
-        let bucket = KiiUser.currentUser()!.bucketWithName(bucketName)
+        let bucket = KiiUser.current()!.bucket(withName: bucketName)
 
         // perform the query
-        bucket.executeQuery(allQuery) { (query, bucket, result, nextQuery, error) -> Void in
+        bucket.execute(allQuery) { (query, bucket, result, nextQuery, error) -> Void in
             // hide the activity indicator(configured "Hides When Stopped" in storyboard)
             self.activityIndicator.stopAnimating()
 
             // check for an error(successful request if error==nil)
             if error != nil {
-                self.showMessage("Query failed", error: error)
+                self.showMessage("Query failed", error: error as NSError?)
                 return
             }
 
             // add the objects to the objectList and display them
-            self.objectList.appendContentsOf(result as! [KiiObject])
+            self.objectList.append(contentsOf: result as! [KiiObject])
             self.tableView.reloadData()
         }
     }
 
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSections(in tableView: UITableView) -> Int {
         // return the number of sections.
         return 1
     }
 
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // return the number of rows in the section.
         return objectList.count
     }
 
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         // initialize a cell
-        var cell: UITableViewCell? = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath:indexPath)
+        var cell: UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "Cell", for:indexPath)
         if cell == nil {
-            cell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"Cell")
+            cell = UITableViewCell(style:UITableViewCellStyle.default, reuseIdentifier:"Cell")
         }
 
         // fill the field from object array
         let obj = objectList[indexPath.row]
-        cell!.textLabel!.text! = obj.getObjectForKey(objectKey) as! String
+        cell!.textLabel!.text! = obj.getForKey(objectKey) as! String
         cell!.detailTextLabel!.text! = obj.objectURI!
         return cell!
     }
 
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath:IndexPath) {
         // show the alert dialog
-        let alert = UIAlertController(title: nil, message: "Would you like to remove this item?", preferredStyle: UIAlertControllerStyle.Alert)
-        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.Default, handler:  { (action) -> Void in
+        let alert = UIAlertController(title: nil, message: "Would you like to remove this item?", preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler:  { (action) -> Void in
             // perform the delete action on the tapped object
             self.performDelete(indexPath.row)
         }))
-        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.Cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: UIAlertActionStyle.cancel, handler: nil))
 
-        presentViewController(alert, animated: true, completion: nil)
+        present(alert, animated: true, completion: nil)
     }
 
     func addItem() {
@@ -128,30 +128,30 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         let value = String(format: "MyObject %d", objectCount)
 
         // get a reference to a KiiBucket
-        let bucket = KiiUser.currentUser()!.bucketWithName(bucketName)
+        let bucket = KiiUser.current()!.bucket(withName: bucketName)
 
         // create a new KiiObject and set a key/value
         let object = bucket.createObject()
         object.setObject(value, forKey: objectKey)
 
         // save the object asynchronoously
-        object.saveWithBlock { (object, error) -> Void in
+        object.save { (object, error) -> Void in
             // hide the activity indicator(configured "Hides When Stopped" in storyboard)
             self.activityIndicator.stopAnimating()
 
             // check for an error(successful request if error==nil)
             if error != nil {
-                self.showMessage("Save failed", error: error)
+                self.showMessage("Save failed", error: error as NSError?)
                 return
             }
 
             // insert the object into the beginning of the objectList and display them
-            self.objectList.insert(object, atIndex: 0)
+            self.objectList.insert(object, at: 0)
             self.tableView.reloadData()
         }
     }
 
-    func performDelete(position: Int) {
+    func performDelete(_ position: Int) {
         // show the activity indicator
         activityIndicator.startAnimating()
 
@@ -159,18 +159,18 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         let obj = objectList[position]
 
         // delete the object synchronously
-        obj.deleteWithBlock { (object, error) -> Void in
+        obj.delete { (object, error) -> Void in
             // hide the activity indicator(configured "Hides When Stopped" in storyboard)
             self.activityIndicator.stopAnimating()
 
             // check for an error(successful request if error==nil)
             if error != nil {
-                self.showMessage("Delete failed", error: error)
+                self.showMessage("Delete failed", error: error as NSError?)
                 return
             }
 
             // remove the object from the list
-            self.objectList.removeAtIndex(position)
+            self.objectList.remove(at: position)
             self.tableView.reloadData()
         }
     }
